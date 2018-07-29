24f69002d25a414ad70a2f6993cf611e5674f15c
Brian Lee <brianjleeofcl@gmail.com>
2018-07-28 19:18:46 -0700

    update code

--- index.js
+++ index.js
@@ -9,25 +9,19 @@ GPhoto.on('log', function (level, domain, message) {
 });
 
 const logPath = path.join(__dirname, "operation.log");
-
 const logger = fs.createWriteStream(logPath, {flags: 'a'});
 
 const takePicture = function(camera) {
   camera.takePicture({keep: true, download: false}, function (er) {
     if (er) {
-      logger.write(`[${Date.now()}]: ERROR: ${er}`)
+      logger.write(`[${Date.now()}]: ERROR: ${er}\r\n`);
     } else {
-      logger.write(`[${Date.now()}]: img #${count} saved`)
+      logger.write(`[${Date.now()}]: img #${count} saved\r\n`);
     }
   });
 }
 
-GPhoto.list(function (list) {
-  console.log('List:', list);
-  if (list.length === 0) return;
-  var camera = list[0];
-  console.log('Found', camera.model);
-
+const main = function(camera) {
   let count = 0
 
   let timer = setInterval(() => {
@@ -36,4 +30,21 @@ GPhoto.list(function (list) {
       takePicture(camera);
       count += 1;
   }, 6000)
+}
+
+GPhoto.list(function (list) {
+  console.log('List:', list);
+  if (list.length === 0) return;
+  var camera = list[0];
+  console.log('Found', camera.model);
+
+  camera.getConfig(function (er) {
+    if (er) {
+      process.exit(1);
+    }
+    else {
+      logger.write('[${Date.now()}]: Starting shoot\r\n')
+      main(camera);
+    }
+  });
 });
